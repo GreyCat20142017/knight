2dac5a23115b659ab2f964ff6e8f5dd020670124
GreyCat20142017 <nrz3siaatg81@mail.ru>
2019-01-06 23:01:30 +0300

    reducers-DRY-оптимизация: редьюсеры разных ходов объединены в универсальный

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,132 +3,68 @@ import { CLICK_CELL, START, CLICK_DISABLED_CELL, CLOSE_MODAL, SHOW_MODAL } from 
 import { getMoveValidity, shuffleArray, getInitialState, getAnalizeResult} from '../functions'
 import { BOARD_SIDE, CONTENT, LIFE_COUNTER_BORDER } from '../constants'
 
-const moveOnEmptyReducer = (state = {}, action) => {
-	let {cells} = state.board;
-	let {score, life} = state.info;
-	switch (action.type) {
-		case CLICK_CELL:
-			const emptyCells = shuffleArray(cells.filter(item => ( item.content === CONTENT.EMPTY) && (item.id !== action.id)  ));
-			let tmpArray  = cells.map((item, ind) => {
-				if (ind === action.id) {
-					item.content = CONTENT.HORSE;
-				}
-				if ((item.content === CONTENT.HORSE) && (ind !== action.id)) {
-					item.content = CONTENT.DISABLED;
-				}
+const onEmptyStarCellMap = (cells, actionId) => {
+ return cells.map((item, ind) => {
+  if (ind === actionId) {
+    item.content = CONTENT.HORSE;
+  }
+  if ((item.content === CONTENT.HORSE) && (ind !== actionId)) {
+    item.content = CONTENT.DISABLED;
+  }
 
-				if (item.content === CONTENT.LITTER)  {
-					item.content = CONTENT.DISABLED;
-				}
-				return item;
-			});
-			if (emptyCells.length > 0) {
-					tmpArray[emptyCells[0].id].content = CONTENT.LITTER;
-			}
-
-			let result = getAnalizeResult(tmpArray, action.id);
-			score = score + result.additionToScore;
-			life = (result.additionToScore) && (score % LIFE_COUNTER_BORDER === 0) ? (life + 1) : life;
-			return {board: {horse: action.id, cells: result.cells}, info: {score: score, life: life}};
-
-		default:	return state;
-	}
+  if (item.content === CONTENT.LITTER)  {
+    item.content = CONTENT.DISABLED;
+  }
+  return item;
+});
 };
 
-const moveOnStarReducer = (state = {}, action) => {
-	let {cells} = state.board;
-	let {score, life} = state.info;
-	score++;
-	switch (action.type) {
-		case CLICK_CELL:
-			const emptyCells = shuffleArray(cells.filter(item => ( item.content === CONTENT.EMPTY) && (item.id !== action.id) ));
-			let tmpArray = cells.map((item, ind) => {
-				if (ind === action.id) {
-					item.content = CONTENT.HORSE;
-				}
-				if ((item.content === CONTENT.HORSE) && (ind !== action.id) ) {
-					item.content = CONTENT.DISABLED;
-				}
-				if (item.content === CONTENT.LITTER)  {
-					item.content = CONTENT.DISABLED;
-				}
-				return item;
-			});
-
-			if (emptyCells.length > 0) {
-					tmpArray[emptyCells[0].id].content = CONTENT.STAR;
-			}
-			if (emptyCells.length > 1) {
-					tmpArray[emptyCells[1].id].content = CONTENT.LITTER;
-			}
-
-			let result = getAnalizeResult(tmpArray, action.id);
-			score = score + result.additionToScore;
-			life = (result.additionToScore) && (score % LIFE_COUNTER_BORDER === 0) ? (life + 1) : life;
-			return {board: {horse: action.id, cells: result.cells}, info: {score: score, life: life}};
-
-		default:	return state;
-	}
+const onLitterCellMap = (cells, actionId) => {
+ return cells.map((item, ind) => {
+  if (ind === actionId) {
+    item.content = CONTENT.HORSE;
+  }
+  if (item.content === CONTENT.HORSE && ind !== actionId) {
+    item.content = CONTENT.DISABLED;
+  }
+  return item;
+});
 };
 
-const moveOnLitterReducer = (state = {}, action) => {
-	let {cells} = state.board;
-	let {score, life} = state.info;
-	switch (action.type) {
-		case CLICK_CELL:
-
-			const emptyCells = shuffleArray(cells.filter(item => (item.content === CONTENT.EMPTY ) && (item.id !== action.id) ));
-			let tmpArray = cells.map((item, ind) => {
-				if (ind === action.id) {
-					item.content = CONTENT.HORSE;
-				}
-				if (item.content === CONTENT.HORSE && ind !== action.id) {
-					item.content = CONTENT.DISABLED;
-				}
-				return item;
-			});
-			if (emptyCells.length > 0) {
-					tmpArray[emptyCells[0].id].content = CONTENT.LITTER;
-			}
-
-			let result = getAnalizeResult(tmpArray, action.id);
-			score = score + result.additionToScore;
-			life = (result.additionToScore) && (score % LIFE_COUNTER_BORDER === 0) ? (life + 1) : life;
-			return {board: {horse: action.id, cells: result.cells}, info: {score: score, life: life}};
-
-		default:	return state;
-	}
+const onLifeLossCellMap = (cells, actionId) => {
+ return cells.map((item, ind) => {
+  if (ind === actionId) {
+    item.content = CONTENT.HORSE;
+  }
+  if (item.content === CONTENT.HORSE && ind !== actionId) {
+    item.content = CONTENT.DISABLED;
+  }
+  return item;
+});
 };
 
-const moveWithLiveLossReducer  = (state = {}, action) => {
- let {cells, horse} = state.board;
- let {score, life} = state.info;
- let tmp = state.modal.tmpHorse || horse;
+const commonMoveReducer = (state = {}, action, specific) => {
+  let {cells, horse} = state.board;
+  let {score, life} = state.info;
+  let actionId = (specific.content === CONTENT.DISABLED) ? (state.modal.tmpHorse || horse) : action.id;
+
+  score = (specific.content === CONTENT.STAR) ? score + 1 : score;
 
   switch (action.type) {
+    case CLICK_CELL:
     case CLICK_DISABLED_CELL:
-      const emptyCells = shuffleArray(cells.filter(item => ( item.content === CONTENT.EMPTY) && (item.id !== tmp)));
-      let tmpArray  = cells.map((item, ind) => {
-
-        if ((item.content === CONTENT.HORSE) && (ind !== tmp)) {
-          item.content = CONTENT.DISABLED;
-        }
-        if ((item.content === CONTENT.LITTER)) {
-          item.content = CONTENT.DISABLED;
-        }
-        if (ind === tmp) {
-          item.content = CONTENT.HORSE;
-        }
-        return item;
-      });
+      const emptyCells = shuffleArray(cells.filter(item => ( item.content === CONTENT.EMPTY) && (item.id !== actionId)  ));
+      let tmpArray  = specific.cellsMap(cells, actionId);
       if (emptyCells.length > 0) {
           tmpArray[emptyCells[0].id].content = CONTENT.LITTER;
       }
-      let result = getAnalizeResult(tmpArray, action.id);
+      if (emptyCells.length > 1 && specific.content === CONTENT.STAR) {
+          tmpArray[emptyCells[1].id].content = CONTENT.STAR;
+      }
+      let result = getAnalizeResult(tmpArray, actionId);
       score = score + result.additionToScore;
       life = (result.additionToScore) && (score % LIFE_COUNTER_BORDER === 0) ? (life + 1) : life;
-      return {board: {horse: tmp, cells: result.cells}, info: {score: score, life: life - 1}, modal: {isModalOpen: false}};
-
+      return {board: {horse: actionId, cells: result.cells}, info: {score: score, life: life - specific.loss}, modal: {isModalOpen: false}};
     default:  return state;
   }
 };
@@ -143,16 +79,16 @@ const reducer = (state = {}, action) => {
 			if (getMoveValidity(horse, action.id)) {
 				switch (clickedContent) {
 					case CONTENT.STAR: {
-						return Object.assign({}, state,  moveOnStarReducer(state, action));
+						return Object.assign({}, state, commonMoveReducer(state, action, {cellsMap: onEmptyStarCellMap, content: clickedContent, loss: 0}));
 					}
 					case CONTENT.LITTER: {
-						return Object.assign({}, state, moveOnLitterReducer(state, action));
+						return Object.assign({}, state,  commonMoveReducer(state, action, {cellsMap: onLitterCellMap, content: clickedContent, loss: 0}));
 					}
 					case CONTENT.DISABLED: {
 						return state;
 					}
 					case CONTENT.EMPTY: {
-						return Object.assign({}, state,  moveOnEmptyReducer(state, action));
+						return Object.assign({}, state,  commonMoveReducer(state, action, {cellsMap: onEmptyStarCellMap, content: clickedContent, loss: 0}));
 					}
 					default: {
             return state;
@@ -168,7 +104,9 @@ const reducer = (state = {}, action) => {
 
     case SHOW_MODAL: return (state.info.life > 0 ) ? Object.assign({}, state, { modal: {isModalOpen: true, tmpHorse: action.id}}) : state;
 
-    case CLICK_DISABLED_CELL: return (state.info.life > 0 ) ?  Object.assign({}, state, moveWithLiveLossReducer(state, action)) : state;
+    case CLICK_DISABLED_CELL: return (state.info.life > 0 ) ?
+      Object.assign({}, state,  commonMoveReducer(state, action, {cellsMap: onLifeLossCellMap, content: CONTENT.DISABLED, loss: 1})) :
+      state;
 
 		default:
 			return state;
